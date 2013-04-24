479e26fb0b9d882430968cef73555320ccb609fb
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-04-24 10:48:05 +0800

    [src/main.c] 以下變更：
    * 引入「C/C++ program templates」專案格式變動
    * 移除參閱 header 檔案的說明，因為主程式通常不會有這種東西...

--- Source_code/main.cpp
+++ Source_code/main.cpp
@@ -1,34 +1,45 @@
 /*
-main.c
 	主程式
-	請見本檔案的 header 檔案以得到更多關於本檔案的說明
+
+	本程式的框架基於「C/C++ program templates」專案
+	This program's frame is based on "C/C++ program templates" project
+		https://github.com/Vdragon/C_CPP_program_templates
 */
-/*||||| 程式所 include 之函式庫的標頭檔 | Included Library Headers |||||*/
-/* Standard C/C++ library definitions */
-	#include <stdlib.h>
-		/* for EXIT_* return code */
-	#include "pauseProgram/Pause_program.h"
-		/* for pausing program execution */
-	#include "Show_software_info/Show_software_info.h"
-		/* for showing software info */
-/*||||| 常數與巨集 | Constants & Macros |||||*/
+/* 程式所 include 之函式庫的標頭檔
+ * Included Library Headers */
+	/* Standard C/C++ library */
+    /* for EXIT_* return code definition */
+		  #include <stdlib.h>
 
-/*||||| Definition of data type, enumeration, data structure and class |||||*/
+  /* Vdragons_C_CPP_Libraries_Collection
+   * https://github.com/Vdragon/Vdragons_C_CPP_Libraries_Collection */
+    /* for pausing program definition */
+		  #include "pauseProgram/Pause_program.h"
+    /* for showing software info */
+		  #include "Show_software_info/Show_software_info.h"
 
-/*||||| 函式雛型 | Function Prototypes |||||*/
+/* 常數與巨集
+ * Constants & Macros */
 
-/*||||| 全域變數 | Global Variables |||||*/
+/* Definition of data type, enumeration, data structure and class */
 
-/*||||| 主要程式碼 | Main Code |||||*/
-int main(int argc, char *argv[]){
-/*用來重新運行程式的label*/
-restart_program:
-	show_software_info("主程式");
+/* 函式雛型
+ * Function Prototypes */
 
-	/*暫停程式運行（於main函式中）*/
-	if(pauseProgram() == 1){
-		goto restart_program;
-	}
+/* 全域變數
+ * Global Variables */
 
-	return EXIT_SUCCESS;
-}
+/* 主要程式碼
+ * Main Code */
+  int main(int argc, char *argv[]){
+  /*用來重新運行程式的label*/
+  restart_program:
+    show_software_info("主程式");
+
+    /*暫停程式運行（於main函式中）*/
+    if(pauseProgram() == 1){
+      goto restart_program;
+    }
+
+    return EXIT_SUCCESS;
+  }
