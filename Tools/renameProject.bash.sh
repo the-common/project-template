9a3e02974644fba88225beda976bb3d161e01b5f
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-05-28 15:56:53 +0800

    Tools/renameProject - 嘗試修正當新專案名稱中有空白時移動專案檔案會失敗的問題

--- Tools/renameProject.bash.sh
+++ Tools/renameProject.bash.sh
@@ -4,7 +4,7 @@ sed 's/C_CPP_project_template/${NAME_PROJECT}/g' Source_code/main.cpp
 sed 's/C_CPP_project_template/${NAME_PROJECT}/g' CMakeLists.txt
 sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Eclipse_CDT/.*project
 sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Code__Blocks/C_CPP_project_template.cbp
-mv IDE_Code__Blocks/C_CPP_project_template.cbp IDE_Code__Blocks/${NAME_PROJECT}.cbp
+mv IDE_Code__Blocks/C_CPP_project_template.cbp "IDE_Code__Blocks/${NAME_PROJECT}.cbp"
 sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_CodeLite/C_CPP_project_template.project
-mv IDE_CodeLite/C_CPP_project_template.project IDE_CodeLite/${NAME_PROJECT}.project
+mv IDE_CodeLite/C_CPP_project_template.project "IDE_CodeLite/${NAME_PROJECT}.project"
 exit 0
