f54a1231fb44f9c0c2f75bce62b23928967d338f
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-05-28 16:00:50 +0800

    Tools/renameProject.bash.sh - 嘗試修正沒有成功重新命名專案的問題

--- Tools/renameProject.bash.sh
+++ Tools/renameProject.bash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read -p "請輸入新的專案名稱：" NAME_PROJECT
-sed 's/C_CPP_project_template/${NAME_PROJECT}/g' Source_code/main.cpp
-sed 's/C_CPP_project_template/${NAME_PROJECT}/g' CMakeLists.txt
-sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Eclipse_CDT/.*project
-sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Code__Blocks/C_CPP_project_template.cbp
+sed --in-place 's/C_CPP_project_template/${NAME_PROJECT}/g' Source_code/main.cpp
+sed --in-place 's/C_CPP_project_template/${NAME_PROJECT}/g' CMakeLists.txt
+sed --in-place 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Eclipse_CDT/.*project
+sed --in-place 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_Code__Blocks/C_CPP_project_template.cbp
 mv IDE_Code__Blocks/C_CPP_project_template.cbp "IDE_Code__Blocks/${NAME_PROJECT}.cbp"
-sed 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_CodeLite/C_CPP_project_template.project
+sed --in-place 's/C_CPP_project_template/${NAME_PROJECT}/g' IDE_CodeLite/C_CPP_project_template.project
 mv IDE_CodeLite/C_CPP_project_template.project "IDE_CodeLite/${NAME_PROJECT}.project"
 exit 0
