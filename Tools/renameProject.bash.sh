7e67ea998676f1a08aeb66bd8f5ea22472b09124
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-03 11:10:20 +0800

    Tools/renameProject 以下變更
    * 改良 main.cpp 改名的正規表達是
    * 新增命令列參數的使用

--- Tools/renameProject.bash.sh
+++ Tools/renameProject.bash.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
-read -p "請輸入新的專案名稱：" NAME_PROJECT
+if [ $# -gt 2 ]; then
+	echo "參數數量錯誤！"
+	exit 1
+elif [ $# -eq 2 ]; then
+	name_project_original="$1"
+	name_project="$2"
+elif [ $# -eq 1 ]; then
+	name_project_original="C_CPP_project_template"
+	name_project="$1"
+else 
+	read -p "請輸入原來的專案名稱（不輸入則使用預設值 C_CPP_project_template）：" name_project_original
+	if [ -z $name_project_original ]; then
+		name_project_original="C_CPP_project_template"
+	fi
+	read -p "請輸入新的專案名稱：" name_project
+	if [ -z $name_project_original ]; then
+		echo "輸入錯誤！"
+		exit 1
+	fi
+fi
 
-sed --in-place "s/C_CPP_project_template/${NAME_PROJECT}/g" CMakeLists.txt
+sed --in-place "s/${name_project_original}/${name_project}/g" CMakeLists.txt
 
-sed --in-place "s/C_CPP_project_template/${NAME_PROJECT}/g" Source_code/main.cpp
+sed --in-place "s/global_software_information.software_name = \"${name_project_original}\";/global_software_information.software_name = \"${name_project}\"/g" Source_code/main.cpp
 
-sed --in-place "s/C_CPP_project_template/${NAME_PROJECT}/g" IDE_Eclipse_CDT/.*project
+sed --in-place "s/${name_project_original}/${name_project}/g" IDE_Eclipse_CDT/.*project
 
-sed --in-place "s/C_CPP_project_template/${NAME_PROJECT}/g" IDE_Code__Blocks/C_CPP_project_template.cbp
-mv IDE_Code__Blocks/C_CPP_project_template.cbp "IDE_Code__Blocks/${NAME_PROJECT}.cbp"
+sed --in-place "s/${name_project_original}/${name_project}/g" IDE_Code__Blocks/${name_project_original}.cbp
+mv IDE_Code__Blocks/${name_project_original}.cbp "IDE_Code__Blocks/${name_project}.cbp"
 
-sed --in-place "s/C_CPP_project_template/${NAME_PROJECT}/g" IDE_CodeLite/C_CPP_project_template.project
-mv IDE_CodeLite/C_CPP_project_template.project "IDE_CodeLite/${NAME_PROJECT}.project"
+sed --in-place "s/${name_project_original}/${name_project}/g" IDE_CodeLite/${name_project_original}.project
+mv IDE_CodeLite/${name_project_original}.project "IDE_CodeLite/${name_project}.project"
 exit 0
