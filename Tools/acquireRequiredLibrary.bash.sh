568dc9438e3460d35fdfdae8982d71b023f95fb0
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-18 01:47:20 +0800

    Tools/acquireRequiredLibrary - 以下變更
    * 將第3方軟體改置放於 3rd_party_software 目錄
    * 改變架構

--- Tools/acquireRequiredLibrary.bash.sh
+++ Tools/acquireRequiredLibrary.bash.sh
@@ -1,17 +1,69 @@
 #!/bin/bash
-mkdir --parent Tools/Tools__temporary_working_directory/
-cd Tools/Tools__temporary_working_directory/
+# 	宣告執行 script 程式用的殼程式(shell)
+################## Header block ##################
+# Script程式名稱
+# Script Name
+# 	獲取本專案所需之函式庫的程式
+# 著作權宣告
+# Copyright Declaration
+# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2014
+# 授權條款宣告
+# License Declaration
+# 	請參閱「C 或 C++ 程式語言專案範本」的說明
+# 傳回值定義
+# Return Value Definition
+# 	0
+# 		正常結束
+# 已知問題
+# Known Issues
+# 	Known issues is now tracked on GitHub
+# 	
+# 修訂紀錄
+# Changelog
+# 	Changelog is now tracked using Git repostiory commit log
+# 	
+############## Header block ended ##############
 
-# 取得 VCCL 函式庫來源程式碼
-git clone https://github.com/Vdragon/Vdragons_C_CPP_Libraries_Collection.git
-cd Vdragons_C_CPP_Libraries_Collection/Buildsystem_CMake
-cmake ..
-make
-cp --force --recursive Build/Libraries/* ../../../../Libraries
-cp --force --recursive Build/Library_headers/* ../../../../Library_headers
-cd ../../../../Libraries
-ln --symbolic */*.a .
+######## Included files ########
 
-cd ../
-rm -rf Tools/Tools__temporary_working_directory
-exit 0
+######## Included files ended ########
+
+######## File scope variable definitions ########
+# idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+readonly PROGRAM_NAME="$(basename $0)"
+readonly PROGRAM_DIRECTORY="$(readlink -m $(dirname $0))"
+readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
+readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+
+######## File scope variable definitions ended ########
+
+######## Program ########
+
+
+# main function, program entry point
+# idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+main() {
+	set -x
+	project_root_path=$(pwd)
+	mkdir --parent 3rd_party_software
+	cd 3rd_party_software
+
+	# 取得 VCCL 函式庫來源程式碼
+	if [ ! -d Vdragon_s_C_CPP_Libraries_Collection ]; then
+		git clone https://github.com/Vdragon/Vdragon_s_C_CPP_Libraries_Collection.git
+	fi
+
+	cd Vdragon_s_C_CPP_Libraries_Collection/Buildsystem_CMake
+	git pull --force
+	cmake ..
+	make
+	cp --force --recursive Build/Libraries/* "${project_root_path}/Libraries"
+	cp --force --recursive Build/Library_headers/* "${project_root_path}/Library_headers"
+	cd "${project_root_path}/Libraries"
+	ln --symbolic */*.a .
+
+	## 正常結束 script 程式
+	exit 0
+	set +x
+}
+main
