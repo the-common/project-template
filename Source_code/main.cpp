7e5f715e928e672a87b9ad7c5334825c006e97f2
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-10-18 09:04:15 +0800

    Tools/renameProject.bash.sh - 修正正規表達式，fixed issue #8
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Source_code/main.cpp
+++ Source_code/main.cpp
@@ -156,7 +156,7 @@ restart_program:
 }
 
 void initializeSoftwareInfo(void) {
-	global_software_information.software_name = "Generic Software Project Template"
+	global_software_information.software_name = "Generic Software Project Template";
 	global_software_information.software_official_website = NULL;
 	global_software_information.software_issue_tracker = NULL;
 	global_software_information.software_developer_name = NULL;
