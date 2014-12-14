4e9afb6658ea81681c88b4af0c66b9824bbe388f
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-12-14 17:14:39 +0800

    Tools/initializeProject.bash.sh - 新增當前工作目錄是否正確的檢查
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Tools/initializeProject.bash.sh
+++ Tools/initializeProject.bash.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+######## File scope variable definitions ########
+# idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+readonly PROGRAM_NAME="$(basename $0)"
+readonly PROGRAM_DIRECTORY="$(readlink -m $(dirname $0))"
+readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
+readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+readonly project_root_path="${PROGRAM_DIRECTORY}/.."
+
+######## File scope variable definitions ended ########
+
+######## Included files ########
+source "${project_root_path}/Tools/checkIllegalUsage.bash.source.sh"
+######## Included files ended ########
+
+checkIllegalUsage
+
 # 移除 Generic Software Project Template 原本的 Git 程式碼倉庫
 rm -rf .git/
 ./Tools/renameProject.bash.sh
