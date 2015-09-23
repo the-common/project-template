3120d645c84339dfba0b227ad805c82db16b5671
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2015-09-24 00:28:32 +0800

    Tools/renameProject.bash.sh - 實作當前工作目錄檢查，解決 #6 議題
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Tools/renameProject.bash.sh
+++ Tools/renameProject.bash.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+######## File scope variable definitions ########
+# idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+readonly PROGRAM_NAME="$(basename $0)"
+readonly PROGRAM_DIRECTORY="$(readlink -m $(dirname $0))"
+readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
+readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+readonly project_root_path="${PROGRAM_DIRECTORY}/.."
+
+######## File scope variable definitions ended ########
+
+######## Included files ########
+source "${project_root_path}/Tools/checkIllegalUsage.bash.source.sh"
+######## Included files ended ########
+
+checkIllegalUsage
+
 name_project_original="Generic Software Project Template"
 
 if [ $# -gt 2 ]; then
